09855ceb7d3f60f481bb6404cbdc7fd924ed4f4a
git <xiaopolin@mylvzuan.com>
2019-11-29 16:27:32 +0800

    update naming rules

--- src/main/java/com/xpl/framework/BaseController.java
+++ src/main/java/com/xpl/framework/BaseController.java
@@ -1,6 +1,6 @@
 package com.xpl.framework;
 
-import com.xpl.api.constant.ErrorCodeConstant;
+import com.xpl.entity.constant.ErrorCodeConstant;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ExceptionHandler;
--- src/main/java/com/xpl/service/ServiceImpl/UserInfoServiceImpl.java
+++ src/main/java/com/xpl/service/ServiceImpl/UserInfoServiceImpl.java
@@ -1,6 +1,6 @@
 package com.xpl.service.ServiceImpl;
 
-import com.xpl.api.po.UserInfoPO;
+import com.xpl.entity.po.UserInfoPO;
 import com.xpl.dao.UserInfoDao;
 import com.xpl.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
