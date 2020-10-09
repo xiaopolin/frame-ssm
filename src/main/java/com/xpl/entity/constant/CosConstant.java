2c7e0c5464322ef70de7c22344182d0a25ea506b
xiaopolin <52851235+xiaopolin@users.noreply.github.com>
2020-10-09 09:08:33 +0800

    Update CosConstant.java

--- src/main/java/com/xpl/entity/constant/CosConstant.java
+++ src/main/java/com/xpl/entity/constant/CosConstant.java
@@ -5,13 +5,13 @@ import com.xpl.framework.BaseEntity;
 
 public class CosConstant extends BaseEntity {
 
-    public static final String SECRET_ID = "AKIDoTvHSQej5D8pJbziU7ZqBIBrITP6sidJ";
+    public static final String SECRET_ID = "";
 
-    public static final String SECRET_KEY = "Y0FI6fz4vYw5zg9QTItHXigRyKUQ8dx4";
+    public static final String SECRET_KEY = "";
 
-    public static final String BUCKET = "jfh-1254006809";
+    public static final String BUCKET = "";
 
-    public static final String REGION_NAME = "ap-guangzhou";
+    public static final String REGION_NAME = "";
 
 
 
